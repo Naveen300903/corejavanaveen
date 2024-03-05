367e9b55cf89a8bf94af9aa73b247cd8aeb861ce
ELCOT <ELCOT@Lenovo>
2024-03-05 04:56:01 -0800

    it is my core java pratical program

---
+++ CoreJava_Naveen/src/Naveen_Java/Naveen.java
@@ -0,0 +1,18 @@
+package Naveen_Java;
+
+import java.util.Scanner;
+
+public class Naveen {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("addition of a="+a+" and b="+b+" is :"+c);
+
+	}
+
+}
